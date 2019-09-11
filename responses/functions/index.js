cde5c47230dfd2291fdb775f62dd13d4b88f11ea
Taylor Caldwell <taycaldwell@users.noreply.github.com>
2019-09-11 15:11:32 -0700

    Remove 📱

--- responses/functions/index.js
+++ responses/functions/index.js
@@ -67,7 +67,7 @@ app.intent('Basic Card', (conv) => {
   conv.ask(`Here's an example of a basic card.`);
   conv.ask(new BasicCard({
     text: `This is a basic card.  Text in a basic card can include "quotes" and
-    most other unicode characters including emoji 📱.  Basic cards also support
+    most other unicode characters including emojis.  Basic cards also support
     some markdown formatting like *emphasis* or _italics_, **strong** or
     __bold__, and ***bold itallic*** or ___strong emphasis___ as well as other
     things like line  \nbreaks`, // Note the two spaces before '\n' required for
